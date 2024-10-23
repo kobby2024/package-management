5ce78569b20010e0ae7c865df27d231246838989
kobby2024 <161094250+kobby2024@users.noreply.github.com>
2024-10-23 06:14:08 -0400

    Update tomcat-install34.sh

--- Tomcat-installation/tomcat-install34.sh
+++ Tomcat-installation/tomcat-install34.sh
@@ -9,18 +9,14 @@ sudo yum install java-1.8.0-openjdk-devel -y
 # install wget unzip packages.
 sudo yum install wget unzip -y
 
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.89/bin/apache-tomcat-9.0.89.zip
-sudo unzip apache-tomcat-9.0.89.zip
-sudo rm -rf apache-tomcat-9.0.89.zip
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.93/bin/apache-tomcat-9.0.93.zip
+sudo unzip apache-tomcat-9.0.93.zip
+sudo rm -rf apache-tomcat-9.0.93.zip
 ### rename tomcat for good naming convention
-sudo mv apache-tomcat-9.0.89/ tomcat9
+sudo mv apache-tomcat-9.0.93 tomcat9
 ### assign executable permissions to the tomcat home directory
-sudo chmod 777 -R /opt/tomcat9
+sudo chmod 775 -R /opt/tomcat9
 sudo chown ec2-user -R /opt/tomcat9
-### Change tomcat port port from 8080 to 8081 
-### curl -v localhost:8080
-### curl -v IP:8080
-### check if tomcat is running with--> ps -ef | grep tomcat 
 ### start tomcat
 sh /opt/tomcat9/bin/startup.sh
 #sh startup.sh = file not found
