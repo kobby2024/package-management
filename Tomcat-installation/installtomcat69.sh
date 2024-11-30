28398600ad19c810d8a0b38299a360dbbef7c2ce
kobby2024 <161094250+kobby2024@users.noreply.github.com>
2024-11-30 01:03:49 -0500

    Update installtomcat69.sh

--- Tomcat-installation/installtomcat69.sh
+++ Tomcat-installation/installtomcat69.sh
@@ -7,7 +7,7 @@ sudo yum install git wget vim -y
 sudo yum install java-1.8.0-openjdk-devel -y
 # Download tomcat software and extract it.
 # dowanload and extract tomcat software
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.97/bin/apache-tomcat-9.0.97.tar.gz
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.97/bin/apache-tomcat-9.0.97.zip
 sudo tar -xvf apache-tomcat-9.0.97.tar.gz
 sudo rm apache-tomcat-9.0.97.tar.gz
 sudo mv apache-tomcat-9.0.97 tomcat9
