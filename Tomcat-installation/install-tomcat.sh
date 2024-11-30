3ecbbfd972a3e06091866a9daf70fa6fc067a3f9
kobby2024 <161094250+kobby2024@users.noreply.github.com>
2024-11-30 00:48:05 -0500

    Update install-tomcat.sh

--- Tomcat-installation/install-tomcat.sh
+++ Tomcat-installation/install-tomcat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # TOMCAT.SH
-# Steps for Installing tomcat9 on rhel7&8
+# Steps for Installing tomcat9 on rhel9
 # install Java JDK 1.8+ as a pre-requisit for tomcat to run.
 # https://github.com/LandmakTechnology/package-management/tree/main/Tomcat-installation
 
@@ -11,11 +11,11 @@ sudo yum install git wget -y
 sudo yum install java-1.8.0-openjdk-devel -y
 # install wget unzip packages.
 sudo yum install wget unzip -y
-sudo wget  https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.89/bin/apache-tomcat-9.0.89 zip
-sudo unzip apache-tomcat-9.0.80.tar.gz
-sudo rm -rf apache-tomcat-9.0.80.tar.gz
+sudo wget  https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.89/bin/apache-tomcat-9.0.97 zip
+sudo unzip apache-tomcat-9.0.97.tar.gz
+sudo rm -rf apache-tomcat-9.0.97.tar.gz
 ### rename tomcat for good naming convention
-sudo mv apache-tomcat-9.0.80 tomcat9
+sudo mv apache-tomcat-9.0.97 tomcat9
 ### assign executable permissions to the tomcat home directory
 sudo chmod 777 -R /opt/tomcat9
 sudo chown ec2-user -R /opt/tomcat9
